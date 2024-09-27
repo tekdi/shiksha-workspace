b3b88e7e3f7affe5df28220360829e23531ed9e6
Arif <arifmajeed007.as@gmail.com>
2024-09-27 15:44:55 +0530

    ckeditor issue fix

--- src/pages/components/QuestionSetEditor.js
+++ src/pages/components/QuestionSetEditor.js
@@ -1,49 +1,31 @@
-// src/pages/components/QuestionSetEditor.js
 import React, { useEffect, useRef } from "react";
-import { questionSetEditorConfig } from "./data"; // Adjust the path as needed
+import { questionSetEditorConfig } from "./data";
 
 const QuestionSetEditor = () => {
   const editorRef = useRef(null);
   let isAppended = false;
 
   useEffect(() => {
-    const loadEditorScript = () => {
-        const script = document.createElement("script");
-        script.src = '/sunbird-questionset-editor-web-component/sunbird-questionset-editor.js';
-        script.async = true;
-
-        script.onload = () => {
-          const editorConfig = questionSetEditorConfig;
-        const questionsetEditorElement = document.createElement("lib-questionset-editor");
-        questionsetEditorElement.setAttribute("editor-config", JSON.stringify(editorConfig));
-
-        questionsetEditorElement.addEventListener("editorEmitter", (event) => {
-          console.log("On editorEvent", event);
-        });
-
-        if (editorRef.current) {
-          editorRef.current.appendChild(questionsetEditorElement);
-          isAppended = true;
-        } else {
-          console.error("Editor ref not available.");
-        }
-      };
-
-      script.onerror = () => {
-        console.error("Failed to load the editor script.");
-      };
-
-      document.body.appendChild(script);
-    };
-
     if (!isAppended) {
-      loadEditorScript();
-    }
+      const editorConfig = questionSetEditorConfig;
+      const questionsetEditorElement = document.createElement(
+        "lib-questionset-editor"
+      );
+      questionsetEditorElement.setAttribute(
+        "editor-config",
+        JSON.stringify(editorConfig)
+      );
 
-    return () => {
-      const scripts = document.querySelectorAll("script[src='/sunbird-questionset-editor-web-component/sunbird-questionset-editor.js']");
-      scripts.forEach(script => script.remove());
-    };
+      questionsetEditorElement.addEventListener("editorEmitter", (event) => {
+        console.log("On editorEvent", event);
+      });
+
+      if (editorRef.current) {
+        console.log("Element appended");
+        editorRef.current.appendChild(questionsetEditorElement);
+        isAppended = true;
+      }
+    }
   }, []);
 
   return <div ref={editorRef}></div>;
