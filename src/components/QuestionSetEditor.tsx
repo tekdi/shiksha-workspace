5f5b25a4b75a08b6105ffd4aa1da39d662442a24
Arif <arifmajeed007.as@gmail.com>
2024-10-09 15:44:20 +0530

    Issue #PS-2167: Integrate create question Set API service in workspace

--- src/components/QuestionSetEditor.tsx
+++ src/components/QuestionSetEditor.tsx
@@ -1,32 +1,175 @@
 import React, { useEffect, useRef } from "react";
-import { questionSetEditorConfig } from "../components/data";
+import { createQuestionSet } from "@/services/ContentService";
+
+const questionSetEditorConfig = {
+  context: {
+    programId: '',
+    contributionOrgId: '',
+    user: {
+      id: 'ef99949b-7f3a-4a5f-806a-e67e683e38f3',
+      fullName: 'Rahul Tekdi',
+      firstName: 'Rahul ',
+      lastName: 'Tekdi',
+      orgIds: ['test-k12-channel']
+    },
+    identifier: '',
+    authToken: ' ',
+    sid: 'iYO2K6dOSdA0rwq7NeT1TDzS-dbqduvV',
+    did: '7e85b4967aebd6704ba1f604f20056b6',
+    uid: 'bf020396-0d7b-436f-ae9f-869c6780fc45',
+    channel: 'test-k12-channel',
+    pdata: {
+      id: 'dev.dock.portal',
+      ver: '2.8.0',
+      pid: 'creation-portal'
+    },
+    contextRollup: {
+      l1: '01307938306521497658',
+    },
+    tags: ['01307938306521497658'],
+    cdata: [
+      {
+        id: '01307938306521497658',
+        type: 'sourcing_organization',
+      },
+      {
+        type: 'project',
+        id: 'ec5cc850-3f71-11eb-aae1-fb99d9fb6737',
+      },
+      {
+        type: 'linked_collection',
+        id: 'do_113140468925825024117'
+      }
+    ],
+    timeDiff: 5,
+    objectRollup: {
+      l1: 'do_113140468925825024117',
+      l2: 'do_113140468926914560125'
+    },
+    host: '',
+    defaultLicense: 'CC BY 4.0',
+    endpoint: '/data/v3/telemetry',
+    env: 'questionset_editor',
+    framework: 'test_k12_framework',
+    cloudStorageUrls: ['https://knowlg-public.s3-ap-south-1.amazonaws.com/'],
+    labels: {
+      save_collection_btn_label: 'Save as Draft',
+    },
+    correctionComments: false,
+    sourcingResourceStatus: true,
+    cloudStorage: {
+      provider: 'azure',
+      presigned_headers: {
+        'x-ms-blob-type': 'BlockBlob'
+      }
+    }
+  },
+  config: {
+    mode: 'edit',
+    enableQuestionCreation: true,
+    enableAddFromLibrary: true,
+    editableFields: {
+      sourcingreview: ['instructions'],
+      orgreview: ['name', 'instructions', 'learningOutcome'],
+      review: ['name', 'description'],
+    },
+    maxDepth: 4,
+    objectType: 'QuestionSet',
+    primaryCategory: 'Practice Question Set',
+    isRoot: true,
+    iconClass: 'fa fa-book',
+    hideSubmitForReviewBtn: false,
+    children: {
+      Question: [
+        'Multiple Choice Question',
+        'Subjective Question'
+      ]
+    },
+    addFromLibrary: false,
+    hierarchy: {
+      level1: {
+        name: 'Section',
+        type: 'Unit',
+        mimeType: 'application/vnd.sunbird.questionset',
+        primaryCategory: 'Practice Question Set',
+        iconClass: 'fa fa-folder-o',
+        children: {},
+        addFromLibrary: true
+      },
+      level2: {
+        name: 'Sub Section',
+        type: 'Unit',
+        mimeType: 'application/vnd.sunbird.questionset',
+        primaryCategory: 'Practice Question Set',
+        iconClass: 'fa fa-folder-o',
+        children: {
+          Question: [
+            'Multiple Choice Question',
+            'Subjective Question'
+          ]
+        },
+        addFromLibrary: true
+      },
+      level3: {
+        name: 'Sub Section',
+        type: 'Unit',
+        mimeType: 'application/vnd.sunbird.questionset',
+        primaryCategory: 'Practice Question Set',
+        iconClass: 'fa fa-folder-o',
+        children: {
+          Question: [
+            'Subjective Question'
+          ]
+        }
+      }
+    },
+    contentPolicyUrl: '/term-of-use.html',
+    assetProxyUrl: '/assets/public/',
+    commonFrameworkLicenseUrl: 'https://creativecommons.org/licenses/'
+  }
+};
 
 const QuestionSetEditor: React.FC = () => {
   const editorRef = useRef<HTMLDivElement | null>(null);
   let isAppended = false;
 
   useEffect(() => {
-    if (!isAppended) {
-      const editorConfig = questionSetEditorConfig;
-      const questionsetEditorElement = document.createElement(
-        "lib-questionset-editor"
-      );
+    const fetchData = async () => {
+      try {
+        const response = await createQuestionSet();
+        const identifier = response?.result?.identifier;
+        console.log('DO ID created successfully:', identifier);
 
-      questionsetEditorElement.setAttribute(
-        "editor-config",
-        JSON.stringify(editorConfig)
-      );
+        questionSetEditorConfig.context.identifier = identifier;
 
-      questionsetEditorElement.addEventListener("editorEmitter", (event: Event) => {
-        console.log("On editorEvent", event);
-      });
+        if (!isAppended && editorRef.current) {
+          const editorConfig = questionSetEditorConfig;
+          const questionsetEditorElement = document.createElement(
+            "lib-questionset-editor"
+          );
 
-      if (editorRef.current) {
-        console.log("Element appended");
-        editorRef.current.appendChild(questionsetEditorElement);
-        isAppended = true;
+          questionsetEditorElement.setAttribute(
+            "editor-config",
+            JSON.stringify(editorConfig)
+          );
+
+          questionsetEditorElement.addEventListener("editorEmitter", (event: Event) => {
+            console.log("On editorEvent", event);
+          });
+
+          if (editorRef.current) {
+            console.log("Element appended");
+            editorRef.current.appendChild(questionsetEditorElement);
+            isAppended = true;
+          }
+        }
+
+      } catch (error) {
+        console.error('Error creating question set:', error);
       }
-    }
+    };
+
+    fetchData();
   }, []);
 
   return <div ref={editorRef}></div>;
--- src/services/ContentService.ts
+++ src/services/ContentService.ts
@@ -1,10 +1,44 @@
 import { post } from "./RestClient";
+import axios from 'axios';
+const authToken = process.env.NEXT_PUBLIC_AUTH_API_TOKEN;
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const getContent = async (reqBody: any) => {
   const apiURL = "https://sunbirdsaas.com/api/content/v1/search";
   try {
     const response = await post(apiURL, reqBody);
     return response?.data?.result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+
+
+export const createQuestionSet = async () => {
+  const apiURL = `${baseUrl}/action/questionset/v2/create`;
+  const reqBody = {
+    request: {
+      questionset: {
+        name: "Test QuestionSet",
+        mimeType: "application/vnd.sunbird.questionset",
+        primaryCategory: "Practice Question Set",
+        code: "de1508e3-cd30-48ba-b4de-25a98d8cfdd2"
+      }
+    }
+  };
+
+
+  try {
+    const response = await axios.post(apiURL, reqBody, {
+      headers: {
+        'Content-Type': 'application/json',
+        'tenantId': 'ef99949b-7f3a-4a5f-806a-e67e683e38f3',
+        'Authorization': `Bearer ${authToken}`,
+      },
+    });
+    return response?.data;
   } catch (error) {
     throw error;
   }
