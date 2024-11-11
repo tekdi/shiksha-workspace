b9e942300928817cab22426a6d41f60f39cff6ba
vivek kasture <vivek_kasture@techjoomla.com>
2024-11-11 17:35:20 +0530

    Issue #PS-0000 fix: Youtube content player fixed

--- src/utils/app.config.ts
+++ src/utils/app.config.ts
@@ -24,4 +24,3 @@ export const MIME_TYPE = {
 export const TENANTID = {
   ID: "ef99949b-7f3a-4a5f-806a-e67e683e38f3",
 };
-https://www.figma.com/design/qTfxC9E9rFMJqZdL0K6JTJ/SCP-Dev-Admin?node-id=841-866&node-type=section&t=0Ufxp3lS0voj13fK-11
