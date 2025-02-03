e834796c10aa0c148f16388acd42dffd406a9af2
Rajnish Dargan <rajnish.code30@gmail.com>
2025-02-03 15:42:05 +0530

    Issue #PS-3338 fix: Adding Other Program

--- src/pages/api/mocked-response.ts
+++ src/pages/api/mocked-response.ts
@@ -372,6 +372,10 @@ export const genericEditorReviewFormResponse = {
                             {
                                 "name": "Open School",
                                 "value": "Open School"
+                            },
+                            {
+                              "name": "Other",
+                              "value": "Other"
                             }
                         ],
                         "placeholder": "Please select an option",
