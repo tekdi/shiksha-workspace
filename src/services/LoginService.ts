77d6f5109d745942b2d7e6a6289bc3339fa9d616
Vivek Kasture <vivek_kasture@techjoomla.com>
2025-02-05 16:17:26 +0530

    Issue #PS-3790 fix: Added Interceptor and handled 401

---
+++ src/services/LoginService.ts
@@ -0,0 +1,16 @@
+import { post } from './RestClient';
+
+interface RefreshParams {
+    refresh_token: string;
+}
+
+export const refresh = async ({refresh_token}: RefreshParams): Promise<any> => {
+    const apiUrl: string = `${process.env.BASE_URL}/user/v1/auth/refresh`;
+    try {
+        const response = await post(apiUrl, { refresh_token });
+        return response?.data;
+    } catch (error) {
+        console.error('error in login', error);
+        throw error;
+    }
+};
